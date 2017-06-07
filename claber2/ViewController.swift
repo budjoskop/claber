9bfaadba5b626fa763640dcbb5c5dcf8b9b9aa37
Ognjen Tomic <gtomicognjen@gmail.com>
2017-06-07 20:48:59 +0200

    Initial Commit

---
+++ claber2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  claber2
+//
+//  Created by Ognjen Tomić on 6/7/17.
+//  Copyright © 2017 Ognjen Tomić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
